ae98b8aa310734d04108e7256bb085502542a374
ISHANSINGH <thapaishan1995@gmail.com>
2023-09-22 23:00:53 +0530

    6th Commit

--- application2.py
+++ application2.py
@@ -11,22 +11,15 @@ from sklearn.metrics import r2_score
 # Load your saved machine learning model
 pipe = pickle.load(open('LinearRegressionModel.pkl', 'rb'))
 
-# Load your car dataset to extract unique manufacturers and models
-car_data = pd.read_csv('Cleaned_Car_data.csv')  # Replace with your dataset path
-
-# Extract unique manufacturers and models
-unique_manufacturers = car_data['company'].unique().tolist()
-unique_models = car_data['name'].unique().tolist()
-
 # Create a Streamlit app
-st.title("Car Price Predictor")
+st.title("Price Predictor")
 
 # Add UI elements for user input
-car_name = st.selectbox("Select Car Name", unique_models)
-car_company = st.selectbox("Select Car Company", unique_manufacturers)
-car_year = st.number_input("Enter Car Year", min_value=1900, max_value=2050, step=1)
-car_kms_driven = st.number_input("Enter KM Driven", min_value=0, max_value=1000000, step=1000)
-fuel_type = st.selectbox("Select Fuel Type", ['Petrol', 'Diesel', 'CNG', 'Electric'])
+car_name = st.selectbox("Car Name")
+car_company = st.selectbox("Car Company")
+car_year = st.number_input("Launch Year", min_value=1980, max_value=2025, step=1)
+car_kms_driven = st.number_input("KM Driven", min_value=0, max_value=1000000, step=100)
+fuel_type = st.selectbox("Fuel Type", ['Petrol', 'Diesel', 'CNG', 'Electric'])
 
 if st.button('Predict Car Price'):
     # Create a DataFrame for prediction
@@ -39,4 +32,4 @@ if st.button('Predict Car Price'):
     # Make predictions
     predicted_price = pipe.predict(input_data)
     
-    st.write(f'Predicted Car Price: ₹{predicted_price[0]:,.2f}')
+    st.write(f'Predicted Car Price: {predicted_price[0]:,.2f}')
